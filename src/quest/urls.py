e0ac0c7b9b9ea3e375c68f3fac2b8c8090098286
Krzysztof Urban <krz.urb@gmail.com>
2011-04-20 03:25:51 +0200

    Beginnings of work on login and register templates; refined logo.

--- src/quest/urls.py
+++ src/quest/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^$', 'quest.views.index'),
     (r'^questionnaire/(?P<quest_id>\d+)/', 'quest.views.questionnaire'),
+    (r'^login/', 'django.contrib.auth.views.login'),
+    (r'^logout/', 'django.contrib.auth.views.logout'),
 )
