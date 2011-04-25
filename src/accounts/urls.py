7d374313a24f000a226bf300419083ba0d2937ae
Krzysztof Urban <krz.urb@gmail.com>
2011-04-26 00:55:42 +0200

    Better logo. Login template is working and styled.

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -4,7 +4,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^login/', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/', 'django.contrib.auth.views.logout', name = 'logout'),
+    url(r'^login/', 'django.contrib.auth.views.login', {}, name='login'),
+    url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/',}, name = 'logout'),
     url(r'^register/', 'accounts.views.register', name='register'),
 )
--- src/settings.py
+++ src/settings.py
@@ -55,6 +55,16 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
+)
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -88,6 +98,8 @@ INSTALLED_APPS = (
     'accounts',
     'quest',
 )
+
+LOGIN_REDIRECT_URL = '/'
 
 INTERNAL_IPS = ('127.0.0.1',)
 
