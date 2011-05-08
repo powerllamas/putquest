667c5713b8e27ca096291a0f6ae379e7a2d6aa03
Krzysztof Urban <krz.urb@gmail.com>
2011-05-08 18:14:49 +0200

    Added conf files for dotcloud.

---
+++ src/dotcloud_settings.py
@@ -0,0 +1,116 @@
+# Django settings for putquest project.
+import os
+PROJECT_ROOT = os.path.dirname(__file__)
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Krzysztof Urban', 'krz.urb@gmail.com'),
+)
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/home/dotcloud/db.sqlite3',
+    }
+}
+
+SECRET_KEY = 'j$o8#^+3^kz6(njsk$1#m1&+ck$3^(s6e173ww(6rom&(hyiv2'
+
+TIME_ZONE = 'Europe/Warsaw'
+
+LANGUAGE_CODE = 'pl-pl'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = ''
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = ''
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+#    'middleware.prettify.BeautifulMiddleware',
+)
+
+ROOT_URLCONF = 'src.urls'
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, 'templates'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.staticfiles',
+
+    'south',
+    'debug_toolbar',
+    'tabs',
+
+    'accounts',
+    'quest',
+)
+
+LOGIN_REDIRECT_URL = '/'
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    ('css','%s/../static/css' % PROJECT_ROOT),
+    ('js','%s/../static/js' % PROJECT_ROOT),
+    ('img','%s/../static/img' % PROJECT_ROOT),
+    ('admin','%s/../static/admin/media' % PROJECT_ROOT),
+)
+
