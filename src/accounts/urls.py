fe2cb5334023bfc6b68a5ef739bd057bb36b994f
Tomek <tomek.zietkiewicz@gmail.com>
2011-05-19 20:12:41 +0200

    Password change added to account settings. Also introduced green color for message about successful change of data - to be discussed.

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -8,4 +8,7 @@ urlpatterns = patterns('',
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/',}, name = 'logout'),
     url(r'^register/', 'accounts.views.register', {}, name='register'),
     url(r'^edit/', 'accounts.views.edit', {}, name='account_edit'),
+	url(r'^pass_change/', 'django.contrib.auth.views.password_change', {}, name = 'pass_change'),
+	url(r'^pass_changed/', 'django.contrib.auth.views.password_change_done', {}, name = 'pass_changed'),
+	
 )
